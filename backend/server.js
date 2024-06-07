56ad98d7f2f8d64e68882ec7c0c5d813a2f9cb95
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2024-06-07 19:48:49 +0330

    ✨ feat: sending protocol
    init basic sending message protocol to server.js by socket.io and emit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const io = require('socket.io')(4000);
 console.log('Listening on port 4000...');
 
 io.on('connection', (socket) => {
-  console.log('new User');
   socket.emit('chat-message', 'Hello from the server!');
+  socket.on('send-chat-message', (message) => {
+    console.log(message);
+  });
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:4000', { transports: ['websocket'] });
+const messageForm = document.getElementById('chat-container');
+const messageInput = document.getElementById('message-input');
 
 socket.on('chat-message', (data) => {
   console.log(data);
+});
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
 });
